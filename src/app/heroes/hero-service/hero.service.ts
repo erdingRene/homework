66b5258312f24a66cd63004057e1d151083af61b
Rene <q@>
2018-12-16 13:49:57 +0100

    expressJS works

--- src/app/heroes/hero-service/hero.service.ts
+++ src/app/heroes/hero-service/hero.service.ts
@@ -12,13 +12,11 @@ export class HeroService {
   constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<IHero[]> {
-    // TODO: send the message _after_ fetching the heroes
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
 
   getHero(id: number): Observable<IHero> {
-    // TODO: send the message _after_ fetching the hero
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
   }
