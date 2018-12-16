bdf263b6de46b3a3df883c83ded6c697fb9bd236
Rene <q@>
2018-12-16 11:51:51 +0100

    Master/Detail Components

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { IHero } from './hero';
+import { HEROES } from './mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+
+export class HeroesComponent implements OnInit {
+
+  heroes = HEROES;
+  selectedHero: IHero;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSelect(hero: IHero): void {
+    this.selectedHero = hero;
+  }
+}
