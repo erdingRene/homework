39250e56d00a3e6b4a8f2901afb609699467f3da
Rene <q@>
2018-12-16 15:24:44 +0100

    heroes array will be send by expressJS and is read by heroService.getheroes

--- src/app/heroes/hero-service/hero.service.ts
+++ src/app/heroes/hero-service/hero.service.ts
@@ -5,16 +5,18 @@ import { Observable, of } from 'rxjs';
 import { IHero } from '../hero';
 import { HEROES } from '../mock-heroes';
 import { MessageService } from '../../messages/message-service/message.service';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({ providedIn: 'root' })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  constructor(private messageService: MessageService,
+              private http: HttpClient) { }
 
-  getHeroes(): Observable<IHero[]> {
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+  getHeroes(): Observable<any> {
+    return this.http.get<any>('http://localhost:3000/api/heroes');
   }
+
 
   getHero(id: number): Observable<IHero> {
     this.messageService.add(`HeroService: fetched hero id=${id}`);
